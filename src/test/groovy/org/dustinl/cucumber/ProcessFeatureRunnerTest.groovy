b075baa2a44b453de667412eb06ec0dbc3aeccb2
Ming Luen Liu <liu.minlun@gmail.com>
2014-12-28 13:05:39 +0800

    cucumberParallelJvm-7 unit test done

--- src/test/groovy/org/dustinl/cucumber/ProcessFeatureRunnerTest.groovy
+++ src/test/groovy/org/dustinl/cucumber/ProcessFeatureRunnerTest.groovy
@@ -9,18 +9,13 @@ class ProcessFeatureRunnerTest extends GroovyTestCase {
     }
 
     void testGetCommand() {
-
-    }
-
-    void testGetOutput() {
-
-    }
-
-    void testGetExitValue() {
-
-    }
-
-    void testGetClasspath() {
-
+        def classpath = '.:/tmp/ewfdf:/var/dfdf/fdsfdsfsdf'
+        def glue = 'classpath:org.dustinl.cucumber'
+        def feature = 'ffffff'
+        def plugins = [new Plugin('pretty'), new Plugin('junit:target/bbb/junit.xml')]
+        ProcessFeatureRunner runners =
+                new ProcessFeatureRunner(classpath: classpath, glue: glue, feature: feature, plugins: plugins)
+        assert runners.command == 'java -cp ' + classpath + ' cucumber.api.cli.Main --glue ' + glue + ' --plugin ' +
+                "pretty --plugin junit:target/bbb/${feature}-junit.xml classpath:" +  feature
     }
 }
